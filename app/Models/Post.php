86f64521e8cd0522bce79b125e5a1f9b8c638b72
K-w-o-n <kwonfsde95@gmail.com>
2024-08-20 11:27:37 +0630

    api

---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title','description'];
+    // or
+    // protected $guarded = [];
+}
